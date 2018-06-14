868531407bdbcebfc6fcaa7b9b9e1048da5b378b
Jolken <jolken122@gmail.com>
2018-06-14 23:08:40 +0300

    add files

---
+++ app/routes/note_routes.js
@@ -0,0 +1,66 @@
+var ObjectID = require('mongodb').ObjectID;
+module.exports = function(app, db) {
+  var database = db.db('notes');
+  app.get('/notes/', (req, res) => {
+      res.sendFile();
+  });
+  app.get('/api/notes/', (req, res) => {
+    const collection = database.collection('notes')
+    collection.find({}).toArray((err, result) => {
+          if (err) {
+              res.send({ 'error' : 'An error has occured'});
+          }
+          else {
+            res.send(result);
+          }
+              
+          });
+  });
+  app.get('/api/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = {'_id': new ObjectID(id)};
+      collection.findOne(details, (err, item) => {
+        if (err) {
+            res.send({ 'error': 'An error has occurred', 'err' : err });
+        }
+        else {
+            res.send(item);
+        }
+      });
+  });
+  app.delete('/api/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = {'_id': new ObjectID(id)};
+      database.collection('notes').remove(details, (err, item) => {
+        if (err) {
+            res.send({ 'error': 'An error has occurred', 'err' : err });
+        }
+        else {
+            res.send('Note ' + id + ' deleted');
+        }
+      });
+  });
+  app.post('/api/notes', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    database.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred', 'err' : err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+  app.put('/api/notes/:id', (req, res) => {
+     const id = req.params.id;
+     const details = { '_id': new ObjectID(id) };
+     const note = { text: req.body.body, title: req.body.title };
+     database.collection('notes').update(details, note, (err, result) => {
+        if (err) {
+            res.send({ 'error': 'An error has occurred', 'err' : err });
+        }
+        else {
+            res.send(note)
+        }
+     });
+  });
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url: 'mongodb://jolken:zzzxxxcv1@ds233320.mlab.com:33320/notes'
+};
