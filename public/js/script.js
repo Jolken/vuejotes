87f2a6b253d1e9e35ccc1378470a8283f3de6e5e
Jolken <jolken122@gmail.com>
2018-06-16 18:20:41 +0300

    cors

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,15 @@
 window.onload = () => {
 //https://agile-badlands-99964.herokuapp.com/api/notes
-
-
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://agile-badlands-99964.herokuapp.com/api/notes', false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send();
+    if (xhr.status != 200) {
+        alert(xhr.status + ': ' + xhr.statusText);
+    } else {
+        alert(xhr.responseText);
+    }
 
 
 
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const db             = require('./config/db');
+const cors           = require('cors');
 const app            = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors());
 MongoClient.connect(db.url, (err, database) => {
+    
     if (err) return console.log(err)
     app.get('/', (req, res) => {
         res.sendFile('index.html', { root: __dirname });
